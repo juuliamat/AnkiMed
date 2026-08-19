c961d975de976221b388882fa686f510a657e375
juuliamat <141352094+juuliamat@users.noreply.github.com>
2026-08-19 23:22:42 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,16 @@
+from aqt import mw
+from aqt.qt import QAction
 
+from .priority_score import show_priority_cards
+
+
+def setup_menu():
+    menu = mw.form.menuTools.addMenu("AnkiMed")
+
+    priority_action = QAction("Priority Score", mw)
+    priority_action.triggered.connect(show_priority_cards)
+
+    menu.addAction(priority_action)
+
+
+setup_menu()
