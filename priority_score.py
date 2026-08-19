0890c93ee1741e4831bfc1b1c8cd84af480d06a8
juuliamat <141352094+juuliamat@users.noreply.github.com>
2026-08-19 23:40:47 +0200

    Update priority_score.py

--- priority_score.py
+++ priority_score.py
@@ -1,1 +1,14 @@
+from aqt import mw
+from aqt.utils import showText
 
+
+def show_priority_cards():
+    rows = mw.col.db.all(
+        """
+        SELECT cid, ease
+        FROM revlog
+        LIMIT 10
+        """
+    )
+
+    showText(str(rows))
