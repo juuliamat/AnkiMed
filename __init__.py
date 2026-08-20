3cde025ca46e3ad4d08e609dba849f9a2e6e9729
juuliamat <141352094+juuliamat@users.noreply.github.com>
2026-08-20 15:58:30 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,88 +1,41 @@
 from aqt import mw
 from aqt.qt import QAction
 
+from .home import show_ankimed_home
 from .priority_score import (
-    show_priority_cards,
     open_priority_cards_in_browser,
+    show_priority_cards,
     start_priority_session,
 )
-
-from .home import show_ankimed_home
+from .weakness_radar import show_weakness_radar
 
 
 def setup_menu():
     menu = mw.form.menuTools.addMenu("AnkiMed")
 
-    # -----------------------------------------------------
-    # AnkiMed Home
-    # -----------------------------------------------------
+    home_action = QAction("AnkiMed Home", mw)
+    home_action.triggered.connect(show_ankimed_home)
+    menu.addAction(home_action)
 
-    home_action = QAction(
-        "AnkiMed Home",
-        mw,
-    )
-
-    home_action.triggered.connect(
-        show_ankimed_home
-    )
-
-    menu.addAction(
-        home_action
-    )
-
-    # Trennlinie
     menu.addSeparator()
 
-    # -----------------------------------------------------
-    # Priority Score
-    # -----------------------------------------------------
+    priority_action = QAction("Priority Score", mw)
+    priority_action.triggered.connect(show_priority_cards)
+    menu.addAction(priority_action)
 
-    priority_action = QAction(
-        "Priority Score",
-        mw,
-    )
+    browser_action = QAction("Priority Cards im Browser öffnen", mw)
+    browser_action.triggered.connect(open_priority_cards_in_browser)
+    menu.addAction(browser_action)
 
-    priority_action.triggered.connect(
-        show_priority_cards
-    )
+    session_action = QAction("Priority Session starten", mw)
+    session_action.triggered.connect(start_priority_session)
+    menu.addAction(session_action)
 
-    menu.addAction(
-        priority_action
-    )
+    menu.addSeparator()
 
-    # -----------------------------------------------------
-    # Priority Cards im Browser
-    # -----------------------------------------------------
-
-    browser_action = QAction(
-        "Priority Cards im Browser öffnen",
-        mw,
-    )
-
-    browser_action.triggered.connect(
-        open_priority_cards_in_browser
-    )
-
-    menu.addAction(
-        browser_action
-    )
-
-    # -----------------------------------------------------
-    # Priority Session
-    # -----------------------------------------------------
-
-    session_action = QAction(
-        "Priority Session starten",
-        mw,
-    )
-
-    session_action.triggered.connect(
-        start_priority_session
-    )
-
-    menu.addAction(
-        session_action
-    )
+    weakness_action = QAction("Weakness Radar", mw)
+    weakness_action.triggered.connect(show_weakness_radar)
+    menu.addAction(weakness_action)
 
 
 setup_menu()
